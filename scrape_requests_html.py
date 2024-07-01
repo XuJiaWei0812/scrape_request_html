3585af147afe5466dc24924ae5266ee8241df506
XuJiaWei0812 <h79jkl345@gmail.com>
2024-07-01 23:47:52 +0800

    Update scrape_requests_html.py

--- scrape_requests_html.py
+++ scrape_requests_html.py
@@ -31,6 +31,7 @@ def parse_list_html(html):
             detail_urls.append(url)
             
         time.sleep(random.uniform(3, 5)) 
+        
         return detail_urls
     except Exception as e:
         error_line = sys.exc_info()[-1].tb_lineno
@@ -45,12 +46,16 @@ def parse_detail_html(html):
 
         # 電影名稱
         detail_data.append(soup.find("h2",class_="m-b-sm").get_text(strip=True) if soup.find("h2",class_="m-b-sm") else None)
+        
         # 上映日期
         detail_data.append(soup.find_all("div",class_="info")[1].find('span').get_text(strip=True).replace(' 上映','') if soup.find_all("div",class_="info")[1].find('span') else None)
+        
         # 電影評分
         detail_data.append(soup.find("p",class_="score").get_text(strip=True) if soup.find("p",class_="score") else None)
+        
         # 電影時長
         detail_data.append(soup.find_all("div",class_="info")[0].find_all('span')[2].get_text(strip=True) if soup.find_all("div",class_="info")[0].find_all('span')[2] else None)
+        
         # 電影簡介
         detail_data.append(soup.find("div",class_="drama").find('p').get_text(strip=True) if soup.find("div",class_="drama").find('p') else None)
         
